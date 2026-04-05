05d3ce067fad431f84bd23bb302517a676f50d87
Mergen <mergen4621@gmail.com>
2026-04-04 22:08:19 -0700

    Add forum section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import Forum from './Forum.tsx'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home.tsx'
 import About from './About.tsx'
 
 function App() {
+
+
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/forum" element={<Forum/>}/>
     </Routes>
   )
 }
