5ae637cba8f01dfb868ec1770034f0db81c948d4
jsun64 <jsun64@students.cs.ubc.ca>
2026-04-04 22:44:19 -0700

    added routing for lessonpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home.tsx'
 import About from './About.tsx'
 import CoursePage from './CoursePage.tsx'
+import LessonPage from './LessonPage.tsx'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/forum" element={<Forum/>}/>
         <Route path="/coursepage" element={<CoursePage/>}/>
+        <Route path="/lessonpage" element={<LessonPage/>}/>
     </Routes>
   )
 }
